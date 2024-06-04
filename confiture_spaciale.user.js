f1e53091d1178c86b11d97a88270767abbcef657
KenshiroiC <gregoirefolios634@gmail.com>
2024-06-04 14:55:11 +0200

    Update confiture_spaciale.user.js

--- confiture_spaciale.user.js
+++ confiture_spaciale.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         confiture_spaciale
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  confiture_spaciale
 // @author       Kenshiroi
 // @match        https://www.youtube.com/*
@@ -11,7 +11,8 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function()
+{
     // Variables
     let isAdFound = false;
     let adLoop = 0;
@@ -21,7 +22,8 @@
     const updateModal = { enable: true, timer: 5000 };
     const scriptUrl = 'https://raw.githubusercontent.com/KenshiroiC/Analayse-Nanochimique-de-la-confiture-dans-l-espace/main/confiture_spaciale.user.js';
 
-    const event = new PointerEvent('click', {
+    const event = new PointerEvent('click',
+    {
         pointerId: 1,
         bubbles: true,
         cancelable: true,
@@ -47,12 +49,15 @@
     });
 
     // Fonction principale pour supprimer les publicités
-    function removeAds() {
-        setInterval(() => {
+    function removeAds()
+    {
+        setInterval(() =>
+        {
             const video = document.querySelector('video');
             const ad = document.querySelector('.ad-showing');
 
-            if (ad) {
+            if (ad)
+            {
                 if (adLoop == 0)
                 {
                     originalVolume = video.volume;
@@ -61,22 +66,24 @@
                 isAdFound = true;
                 adLoop += 1;
 
-                if (adLoop >= 2 && video.currentTime > 0.10) {
+                if (adLoop >= 2 && video.currentTime > 0.10)
+                {
                     let randomNumber = Math.floor(Math.random() * 2) + 1;
                     video.playbackRate = 10 - randomNumber;
                 }
 
                 skipAd();
                 video.play();
-            } else {
-                handleVideoPlayback(video);
             }
+            else handleVideoPlayback(video);
         }, 50);
     }
 
     // Fonction pour ignorer les publicités
-    function skipAd() {
-        const skipButtons = [
+    function skipAd()
+    {
+        const skipButtons =
+        [
             '.ytp-ad-skip-button-container',
             '.ytp-ad-skip-button-modern',
             '.videoAdUiSkipButton',
@@ -88,40 +95,48 @@
             '#skip-button'
         ];
 
-        skipButtons.forEach(selector => {
-            document.querySelectorAll(selector).forEach(element => {
-                element.dispatchEvent(event);
-            });
+        skipButtons.forEach(selector =>
+        {
+            document.querySelectorAll(selector).forEach(element => { element.dispatchEvent(event); });
         });
     }
 
     // Fonction pour gérer la lecture de la vidéo
-    function handleVideoPlayback(video) {
-        if (video && video.playbackRate === 10) {
+    function handleVideoPlayback(video)
+    {
+        if (video && video.playbackRate === 10)
+        {
             video.playbackRate = videoPlayback;
-            video.volume = originalVolume;
         }
 
-        if (isAdFound) {
+        if (isAdFound)
+        {
             isAdFound = false;
             videoPlayback = videoPlayback === 10 ? 1 : videoPlayback;
             if (video && isFinite(videoPlayback)) video.playbackRate = videoPlayback;
             adLoop = 0;
-        } else {
+            video.volume = originalVolume;
+        }
+        else
+        {
             if (video) videoPlayback = video.playbackRate;
         }
     }
 
     // Fonction pour vérifier les mises à jour du script
-    function checkForUpdate() {
+    function checkForUpdate()
+    {
         fetch(scriptUrl)
-            .then(response => {
+            .then(response =>
+            {
                 if (!response.ok) throw new Error("Network response was not ok");
                 return response.text();
             })
-            .then(content => {
+            .then(content =>
+            {
                 const match = content.match(/@version\s+(\d+\.\d+)/);
-                if (!match) {
+                if (!match)
+                {
                     console.error("Unable to extract version from the GitHub script.");
                     return;
                 }
@@ -129,21 +144,23 @@
                 const githubVersion = parseFloat(match[1]);
                 const currentVersion = parseFloat(GM_info.script.version);
 
-                if (githubVersion <= currentVersion) {
+                if (githubVersion <= currentVersion)
+                {
                     console.log(`You have the latest version of the script. ${githubVersion} : ${currentVersion}`);
                     return;
                 }
 
                 console.log(`NO PUB YTP: A new version is available. Please update your script. ${githubVersion} : ${currentVersion}`);
 
-                if (updateModal.enable) {
+                if (updateModal.enable)
+                {
                     if (parseFloat(localStorage.getItem('skipNoPubYTPVersion')) === githubVersion) return;
                     loadSweetAlert(githubVersion);
-                } else {
-                    confirmUpdate();
                 }
+                else confirmUpdate();
             })
-            .catch(error => {
+            .catch(error =>
+            {
                 hasIgnoredUpdate = true;
                 console.error("Error checking for updates:", error);
             });
@@ -151,7 +168,8 @@
     }
 
     // Fonction pour charger SweetAlert
-    function loadSweetAlert(githubVersion) {
+    function loadSweetAlert(githubVersion)
+    {
         const script = document.createElement('script');
         script.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11';
         document.head.appendChild(script);
@@ -160,8 +178,10 @@
         style.textContent = '.swal2-container { z-index: 2400; }';
         document.head.appendChild(style);
 
-        script.onload = function () {
-            if (typeof Swal !== 'undefined') {
+        script.onload = function ()
+        {
+            if (typeof Swal !== 'undefined')
+            {
                 Swal.fire({
                     position: "top-end",
                     backdrop: false,
@@ -174,33 +194,28 @@
                     cancelButtonText: 'Close',
                     timer: updateModal.timer ?? 5000,
                     timerProgressBar: true,
-                    didOpen: (modal) => {
+                    didOpen: (modal) =>
+                    {
                         modal.onmouseenter = Swal.stopTimer;
                         modal.onmouseleave = Swal.resumeTimer;
                     }
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.replace(scriptUrl);
-                    } else if (result.isDenied) {
-                        localStorage.setItem('skipNoPubYTPVersion', githubVersion);
-                    }
+                }).then((result) =>
+                {
+                    if (result.isConfirmed) window.location.replace(scriptUrl);
+                    else if (result.isDenied) localStorage.setItem('skipNoPubYTPVersion', githubVersion);
                 });
-            } else {
-                console.error("SweetAlert2 not loaded properly.");
             }
+            else console.error("SweetAlert2 not loaded properly.");
         };
 
-        script.onerror = function () {
-            confirmUpdate();
-        }
+        script.onerror = function () { confirmUpdate(); }
     }
 
     // Fonction pour confirmer la mise à jour
-    function confirmUpdate() {
+    function confirmUpdate()
+    {
         const result = window.confirm("NO PUB YTP: A new version is available. Please update your script.");
-        if (result) {
-            window.location.replace(scriptUrl);
-        }
+        if (result) window.location.replace(scriptUrl);
     }
 
     checkForUpdate();
